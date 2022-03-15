a005f1f80138cb19e10ae7672157f21c68f78aa0
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2022-03-15 13:03:42 +0530

    js module 2 task 5

--- js-module-2/app.js
+++ js-module-2/app.js
@@ -52,4 +52,61 @@ console.log("twice of each number: ", twiceNum);
 const halfNum = task4.map(value => value/2)
 console.log("Half Num:", halfNum);
 
+// 5. Provided the following  array:
+//     [10, 1, 42, 36, 4, 75, 6, 97, 81, 19, 10]
+//   a. Find the sum, min, and max
+//   b. Print “List is full of prime no” if every element is prime
+//   c. Print “List has a prime no” if there is a prime no in it
+//   d. Remove duplicate values from the list.
 
+// Note: Use map(), filter(), reduce(), some() and every() methods for above problems
+
+//  e. Remove duplicate values from the list without using loop or any of 
+// the above methods (map(), filter(), reduce(), some() and every())
+
+
+const task5 = [10, 1, 42, 36, 4, 75, 6, 97, 81, 19, 10]
+console.log("Task 5 list:", task5);
+
+// a1. Find Sum -
+const sumTask = task5.reduce((current, next) => { return current + next })
+console.log("Task 5 Sum:", sumTask);
+
+// a2. Find Min -
+const TaskMin = task5.reduce((prev, current) => { return prev < current ? prev : current})
+console.log(TaskMin);
+
+// a3. Find Max -
+const Taskmax = task5.reduce((prev, current) => { return prev > current ? prev : current })
+console.log(Taskmax);
+
+// Resuable Function -
+function onePrime(num){
+    let value = true
+
+    for(let i = 2; i < num; i++){
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return num ? true : false
+}
+
+// b. List Full of prime -
+const primeList = task5.every(onePrime);
+console.log("List have all prime numbers: ", primeList);
+
+// c. List has a prime number -
+const hasPrime = task5.some(onePrime);
+console.log("List has a prime number: ", hasPrime);
+
+// d. Remove Duplicates -
+const duplicates = task5.filter((item, index) => {
+    return task5.indexOf(item) == index;
+})
+
+console.log("Removed Duplicates", duplicates);
+
+// e. Remove Duplicates without loops or any above method -
+const dupsSet = new Set(task5);
+console.log("Rmoved Duplicates using set object:", dupsSet);
