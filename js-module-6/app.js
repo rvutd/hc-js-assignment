42580ddc763ea62e88357426e70937498cb079e8
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2022-03-17 10:40:15 +0530

    JS Module 6 Task 1

---
+++ js-module-6/app.js
@@ -0,0 +1,10 @@
+const nameColElements = document.querySelectorAll('.name');
+
+// Click Anywhere in column to change its color -
+nameColElements.forEach(element => {
+    element.addEventListener('click', () => {
+        nameColElements.forEach(colElement => {
+            colElement.classList.toggle('change');
+        })
+    })
+})
